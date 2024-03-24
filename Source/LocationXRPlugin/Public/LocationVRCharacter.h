e8c369c392fe7870f1d59d47ea2189530d079d80
aaronp51 <petrekaj@gmail.com>
2024-03-24 08:56:29 -0700

    Project upload

---
+++ Source/LocationXRPlugin/Public/LocationVRCharacter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "VRCharacter.h"
+#include "LocationVRCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LOCATIONXRPLUGIN_API ALocationVRCharacter : public AVRCharacter
+{
+	GENERATED_BODY()
+	
+};
